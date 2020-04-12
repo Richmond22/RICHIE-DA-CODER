b08af8f18ded5c1e749bd58fa5791d0204a3a943
Richmond22 <richmond@i-pay.co.za>
2020-04-12 18:29:17 +0200

    Initial commit

---
+++ Voice.Control/Voice.Control/MainActivity.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Linq;
+using Android.App;
+using Android.Bluetooth;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Speech;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Java.Util;
+using AlertDialog = Android.App.AlertDialog;
+
+namespace Voice.Control
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private bool isRecording;
+        private readonly int VOICE = 10;
+        private TextView textBox;
+        private Button recButton;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            isRecording = false;
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            textBox = FindViewById<TextView>(Resource.Id.search_voice_btn);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            var adapter = BluetoothAdapter.DefaultAdapter;
+            var device = adapter.BondedDevices.FirstOrDefault(x => x.Name == "Richmond’s iPhone");
+
+            var _socket = device.CreateRfcommSocketToServiceRecord(UUID.FromString("00001101-0000-1000-8000-00805f9b34fb"));
+            var t = _socket.ConnectAsync();
+            t.GetAwaiter().GetResult();
+            var s = t.Status;
+
+            // Read data from the device
+            //await _socket.InputStream.ReadAsync(buffer, 0, buffer.Length);
+
+            //// Write data to the device
+            //await _socket.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View)sender;
+
+            string rec = Android.Content.PM.PackageManager.FeatureMicrophone;
+
+            if (rec != "android.hardware.microphone")
+            {
+                var alert = new AlertDialog.Builder(recButton.Context);
+                alert.SetTitle("You don't seem to have a microphone to record with");
+                alert.SetPositiveButton("OK", (sender, e) =>
+                {
+                    textBox.Text = "No microphone present";
+                    return;
+                });
+
+                alert.Show();
+            }
+            else
+
+                isRecording = !isRecording;
+            if (isRecording)
+            {
+                var voiceIntent = new Intent(RecognizerIntent.ActionRecognizeSpeech);
+                voiceIntent.PutExtra(RecognizerIntent.ExtraLanguageModel, RecognizerIntent.LanguageModelFreeForm);
+                voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputCompleteSilenceLengthMillis, 1500);
+                voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputPossiblyCompleteSilenceLengthMillis, 1500);
+                voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputMinimumLengthMillis, 15000);
+                voiceIntent.PutExtra(RecognizerIntent.ExtraMaxResults, 1);
+
+                voiceIntent.PutExtra(RecognizerIntent.ExtraLanguage, Java.Util.Locale.Default);
+                StartActivityForResult(voiceIntent, VOICE);
+            }
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultVal, Intent data)
+        {
+            if (requestCode == VOICE)
+            {
+                if (resultVal == Result.Ok)
+                {
+                    var matches = data.GetStringArrayListExtra(RecognizerIntent.ExtraResults);
+                    if (matches.Count != 0)
+                    {
+                        var textInput = matches[0];
+
+                        // limit the output to 500 characters
+                        if (textInput.Length > 500)
+                            textInput = textInput.Substring(0, 500);
+                        textBox.Text = textInput;
+                    }
+                    else
+                        textBox.Text = "No speech was recognised";
+                }
+            }
+
+            base.OnActivityResult(requestCode, resultVal, data);
+        }
+
+    }
+}
+
